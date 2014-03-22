6d4bdf1f2fa11fc7fe2dd3b1572585342b03cb77
rkttu <rkttu@rkttu.com>
2014-03-22 11:21:55 +0900

    Test Application

---
+++ OpenHDKSharp/OpenHDKSharp.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenHDKSharp.V3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenHDKSharp.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HwpV3Document doc = HwpV3Document.Load("v3.hwp");
+            foreach (var eachPara in doc.Paragraph)
+                Console.WriteLine(eachPara.ToString());
+        }
+    }
+}
