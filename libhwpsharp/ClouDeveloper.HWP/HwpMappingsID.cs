60c6a3dc33bf4d28a253b2ce6393feebc59a831e
Jung Hyun, Nam <rkttu@rkttu.com>
2016-01-03 01:17:17 +0900

    hwp-enums.h implementation added

---
+++ libhwpsharp/ClouDeveloper.HWP/HwpMappingsID.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClouDeveloper.HWP
+{
+    public enum HwpMappingsID
+    {
+        ID_BINARY_DATA = 0,
+        ID_KOREAN_FONTS = 1,
+        ID_ENGLISH_FONTS = 2,
+        ID_HANJA_FONTS = 3,
+        ID_JAPANESE_FONTS = 4,
+        ID_OTHERS_FONTS = 5,
+        ID_SYMBOL_FONTS = 6,
+        ID_USER_FONTS = 7,
+        ID_BORDER_FILLS = 8,
+        ID_CHAR_SHAPES = 9,
+        ID_TAB_DEFS = 10,
+        ID_PARA_NUMBERINGS = 11,
+        ID_BULLETS = 12,
+        ID_PARA_SHAPES = 13,
+        ID_STYLES = 14,
+        ID_MEMO_SHAPES = 15,
+        ID_UNKNOWN_16 = 16,
+        ID_UNKNOWN_17 = 17,
+    }
+}
